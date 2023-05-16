257e8a1fe57eb6bfd11fae5f315947eea6951ecd
Tove Karlström <tovekarlstrom@hotmail.se>
2023-05-16 15:12:05 +0200

    login fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
-import Footer from './components/Footer';
-import MicroNutrient from './components/MicroNutrients';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <MicroNutrient></MicroNutrient>
-     <Footer></Footer>
-    </div>
-  );
-import React from "react"
-import { createHashRouter, RouterProvider } from "react-router-dom"
-import Root from "./Root"
-import "./App.css"
-=======
 import React from "react";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import Root from "./Root";
 import "./App.css";
->>>>>>> 2a711fcc78730458e2b5b699ef309408ad8318a7
 
 import Login from "./components/Login";
 import HomeView from "./view/HomeView";
