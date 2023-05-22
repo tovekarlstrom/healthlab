ab550f45392fbc3be56812672810ac6b103092a3
Tove Karlström <tovekarlstrom@hotmail.se>
2023-05-22 12:39:18 +0200

    added review component and added styling

--- frontend/src/view/RecipeView.tsx
+++ frontend/src/view/RecipeView.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Recipe } from "./HomeView";
 import ArrowButton from "../components/ArrowButton";
 import StarRating from "../components/StarRating";
@@ -8,6 +9,7 @@ import { Clock } from "react-bootstrap-icons";
 import { Heart } from "react-bootstrap-icons";
 import { ChatText } from "react-bootstrap-icons";
 import { Bag } from "react-bootstrap-icons";
+import Review from "../components/Review";
 
 function RecipeView() {
   const { recipeName } = useParams();
@@ -91,6 +93,45 @@ function RecipeView() {
                 <li key={index}>{item}</li>
               ))}
             </ul>
+            <div className="recipeReviews">
+              <div className="IconHeading">
+                <div className="greenIconBox">
+                  <ChatText className="IconChat" />
+                </div>
+                <h2 className="recipeH2">Recensioner</h2>
+              </div>
+              <Review
+                name="Jane Doe"
+                rating={3.5}
+                comment="Den här appen har gjort det mycket enklare att gå ner i vikt!"
+                image="YasminFrost.png"
+              />
+              <Review
+                name="Jane Doe"
+                rating={3.5}
+                comment="Den här appen har gjort det mycket enklare att gå ner i vikt!"
+                image="YasminFrost.png"
+              />
+              <Review
+                name="Jane Doe"
+                rating={3.5}
+                comment="Den här appen har gjort det mycket enklare att gå ner i vikt!"
+                image="YasminFrost.png"
+              />
+            </div>
+            <div className="LogginIfNot">
+              <p>
+                Du måste vara inloggad för att skriva en recension,{" "}
+                <Link className="commentLink" to="/login">
+                  logga in
+                </Link>{" "}
+                eller{" "}
+                <Link className="commentLink" to="/register">
+                  registrera dig
+                </Link>
+                .
+              </p>
+            </div>
           </div>
         </div>
       )}
